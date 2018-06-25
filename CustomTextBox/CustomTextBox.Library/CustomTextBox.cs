0ed8ea8b1a7918787203e1a4deab946cedc4d3d6
Sait ORHAN <saitorhan@outlook.com>
2018-06-25 14:03:51 +0300

    textbox

---
+++ CustomTextBox/CustomTextBox.Library/CustomTextBox.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CustomTextBox.Library
+{
+    public partial class CustomTextBox : TextBox
+    {
+        public DataType DataType { get; set; }
+        public CustomTextBox()
+        {
+            InitializeComponent();
+            DataType = DataType.NumberLetter;
+            KeyPress += OnKeyPress;
+        }
+
+        private void OnKeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsControl(e.KeyChar) && (DataType == DataType.Number ? !char.IsDigit(e.KeyChar) : DataType == DataType.Letter ? !Char.IsLetter(e.KeyChar) : !Char.IsLetterOrDigit(e.KeyChar)) &&
+                (e.KeyChar != '.'))
+            {
+                e.Handled = true;
+            }
+
+            // only allow one decimal point
+            if ((e.KeyChar == '.') && ((sender as TextBox).Text.IndexOf('.') > -1))
+            {
+                e.Handled = true;
+            }
+        }
+    }
+
+    public enum DataType
+    {
+        Number,
+        Letter,
+        NumberLetter
+    }
+}
---
+++ CustomTextBox/CustomTextBox.Test/Form1.Designer.cs
@@ -0,0 +1,60 @@
+﻿namespace CustomTextBox.Test
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.customTextBox1 = new CustomTextBox.Library.CustomTextBox();
+            this.SuspendLayout();
+            // 
+            // customTextBox1
+            // 
+            this.customTextBox1.DataType = CustomTextBox.Library.DataType.NumberLetter;
+            this.customTextBox1.Location = new System.Drawing.Point(12, 12);
+            this.customTextBox1.Name = "customTextBox1";
+            this.customTextBox1.Size = new System.Drawing.Size(100, 20);
+            this.customTextBox1.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(439, 261);
+            this.Controls.Add(this.customTextBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Library.CustomTextBox customTextBox1;
+    }
+}
+
